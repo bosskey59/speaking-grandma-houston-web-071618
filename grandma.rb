ad38707037eac6e059418d29650ffcdb8ac3a095
Alejandro Aguilar <alejandro.aguilar.jr@gmail.com>
2018-06-23 19:10:39 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,6 @@ def speak_to_grandma(phrase)
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif (phrase== phrase.upcase)
     puts "NO, NOT SINCE 1938!"
-  end
   else
     puts " HUH?! SPEAK UP, SONNY!"
 end
